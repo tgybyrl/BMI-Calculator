53f89241d81be185bc5ef1d3b8e7813841e67e58
Tugay Bayırlı <tgybyrl48@gmail.com>
2024-11-22 14:33:02 +0300

    digit validation added

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -14,30 +14,39 @@ weight_entry.focus()
 weight_entry.pack()
 
 
-#entry
+#height label - entry
 height_label = Label(text="Enter Your Height(cm)",font=FONT)
 height_label.pack()
 height_entry = Entry(width=20)
 height_entry.pack()
 
+#weight validation
+def weight_validation(weight,height):
+    if weight.isdigit() and height.isdigit():
+        return True
+    return False
+
 #button
 def button_calculater():
-    global result_bmi
-    calculater =(int(weight_entry.get())) / (((int(height_entry.get()))/100)**2)
-    if calculater <= 18.5:
-        result_bmi = f"Your BMI score {calculater} = Underweight"
-    elif 18.5 < calculater <= 25:
-        result_bmi = f"Your BMI score {calculater} = Normal Weight"
-    elif 25 < calculater <= 29.9:
-        result_bmi = f"Your BMI score {calculater} = Overweight"
-    elif calculater >= 30:
-        result_bmi = f"Your BMI score {calculater} = Obese"
-    result_label = Label(text=result_bmi)
-    result_label.pack()
+    weight = weight_entry.get()
+    height = height_entry.get()
+    if not weight_validation(weight,height):
+        messagebox = Label(text="Please enter only digit!")
+        messagebox.pack()
+    else:
+        global result_bmi
+        calculater =(int(weight_entry.get())) / (((int(height_entry.get()))/100)**2)
+        if calculater <= 18.5:
+            result_bmi = f"Your BMI score {calculater} = Underweight"
+        elif 18.5 < calculater <= 25:
+            result_bmi = f"Your BMI score {calculater} = Normal Weight"
+        elif 25 < calculater <= 29.9:
+            result_bmi = f"Your BMI score {calculater} = Overweight"
+        elif calculater >= 30:
+            result_bmi = f"Your BMI score {calculater} = Obese"
+        result_label = Label(text=result_bmi)
+        result_label.pack()
 
-"""    result_label = Label(text=result_bmi)
-    result_label.pack()
-    result_label.update()"""
 the_button = Button(text="Calculate",command=button_calculater)
 the_button.pack()
 
